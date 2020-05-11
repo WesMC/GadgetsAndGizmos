6afbf81aa914aa2fc50b8b72d9b4cc9163a062f1
WesMC <wesley.couturier@gmail.com>
2020-05-11 17:23:44 -0400

    Separated DataAccessLayer, Models, and Utilities from main project.

--- GadgetsAndGizmos/Startup.cs
+++ GadgetsAndGizmos/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using GadgetsAndGizmos.Data;
+using GadgetsAndGizmos.DataAccessLayer.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,9 +30,9 @@ namespace GadgetsAndGizmos
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
         }
 
